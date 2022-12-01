f79b52a0ab2dd43ac573fb57fb9df49aa900e776
ohhyunrok <111178599+ohhyunrok@users.noreply.github.com>
2022-12-01 15:41:28 +0900

    ..

---
+++ src/main/java/com/kh3/admin/controller/AdminCouponController.java
@@ -0,0 +1,21 @@
+package com.kh3.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.kh3.model.coupon.CouponDAO;
+
+
+
+@Controller
+public class AdminCouponController {
+
+	@Autowired
+	private CouponDAO dao;
+	
+	
+	
+	
+	
+
+}
