cde97480649af0718aab3c0404781f46bb31a29b
Dongjin, Noh <ndj11111@gmail.com>
2022-12-19 19:33:43 +0900

    Update SiteFicnicController.java

--- src/main/java/com/kh3/site/controller/SiteFicnicController.java
+++ src/main/java/com/kh3/site/controller/SiteFicnicController.java
@@ -3,6 +3,7 @@ package com.kh3.site.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
