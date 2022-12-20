90e25e90bb292a3bf034c865e2db5ce91e25799a
bbbmore <icebear0425@naver.com>
2022-12-20 16:26:07 +0900

    ㅠㅠㅠ
    ㅠㅠㅠ

--- src/main/java/com/kh3/site/controller/SiteFicnicController.java
+++ src/main/java/com/kh3/site/controller/SiteFicnicController.java
@@ -327,36 +327,51 @@ public class SiteFicnicController {
     // =====================================================================================
     @RequestMapping("ficnic/ficnic_review.do")
     public String ficnic_review(@RequestParam(value = "ficnic_no", required = false, defaultValue = "") int ficnic_no,
-    		@RequestParam(value = "getType", required = false, defaultValue = "") String getType,
-    		Model model,
-    		HttpServletRequest request) {
+    		@RequestParam(value = "getType", required = false, defaultValue = "") String getType, 
+    		@RequestParam(value = "page", required = false, defaultValue = "1") int page, Model model, HttpServletRequest request) {
         
-    	Map<String, Object> map = new HashMap<String, Object>();
-    	map.put("ficnic_no",ficnic_no);
-    	map.put("getType", getType);
+    	// 정렬 설정
+    	Map<String, Object> searchMap = new HashMap<String, Object>();
+    	searchMap.put("ficnic_no", ficnic_no);
+    	searchMap.put("getType", getType);
+    	// 페이징
+    	totalRecord = this.rdao.getSiteReviewCount(searchMap);
+
+		PageDTO pdto = new PageDTO(page, rowsize, totalRecord, searchMap);
+		
+		// 정렬 쿼리
+		List<ReviewDTO> rList = rdao.getNumList(pdto.getStartNo(), pdto.getEndNo(), searchMap);
+		System.out.println("rlist>>>" + rList);
+		System.out.println("pdto>>>" + pdto.getStartNo());
+
+		// 페이지 이동 URL
+		String pageUrl = request.getContextPath()+"ficnic/ficnic_review.do?ficnic_no="+ficnic_no+"&getType="+getType+"&page="+page;
+		
+
     	FicnicDTO fdto = fdao.getFicnicCont(ficnic_no);
-        
-        List<ReviewDTO> rList = rdao.getNumList(map);
 		int count = fdao.countAll(ficnic_no);
 		int rcount = fdao.countReviewPoint(ficnic_no);
-        
+     
+	    
         model.addAttribute("fdto", fdto);
         model.addAttribute("rList", rList);
 		model.addAttribute("count", count);
 		model.addAttribute("rcount", rcount);
 		
-    
+		model.addAttribute("paging", pdto);
+		model.addAttribute("page", page);
+		model.addAttribute("getType", getType);
+		model.addAttribute("pagingWrite",Paging.showPage(pdto.getAllPage(), pdto.getStartBlock(), pdto.getEndBlock(), pdto.getPage(), pageUrl));
 		
-		
-		
-		
-		
-		
-		
+		System.out.println("확인1>>>>>>>" + pdto.getAllPage());
+		System.out.println("확인2>>>>>>>" + pdto.getStartBlock());
+		System.out.println("확인3>>>>>>>" + pdto.getEndBlock());
+		System.out.println("확인4>>>>>>>" + pdto.getPage());
+		System.out.println("확인5>>>>>>>" + pageUrl);
 		
         return "site/ficnic/ficnic_review";
+        
     }
-
 
 
 
